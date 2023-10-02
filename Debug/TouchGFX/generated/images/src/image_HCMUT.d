8a0954560d26ba8c3606f44f8237982cdf5a2a83
Anh Vu <anv2003bmt@gmail.com>
2023-10-02 19:45:42 +0700

    init tft lcd

---
+++ Debug/TouchGFX/generated/images/src/image_HCMUT.d
@@ -0,0 +1,4 @@
+TouchGFX/generated/images/src/image_HCMUT.o: \
+ ../TouchGFX/generated/images/src/image_HCMUT.cpp \
+ ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:
